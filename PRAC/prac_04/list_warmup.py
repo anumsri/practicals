97dec07671bfd325a5ebdd9710e9537e26d2c5e7
ArpisittNumsri <you@example.com>
2021-08-24 11:03:54 +1000

    List Warmup

---
+++ PRAC/prac_04/list_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""""
+ 1. 0 gives the 3
+ 2. -1 gives 2 
+ 3. 3 gives 1
+ 4.  :-1 gives 3 - 9
+ 5. 3:4 gives 1-5
+ 6. 5 in numbers give true
+ 7. 7 in numbers give false
+ 8. 8 gives false
+ 9. prints all numbers + 6,5,3
+"""""
+
+# 1. numbers[0] = "ten"
+# 2. numbers[-1] = 1
+# 3. print(numbers[2:])
+# 4. 9 in numbers 
+
