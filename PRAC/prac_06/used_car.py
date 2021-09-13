c68d2c60aed3b6fcac514e1f97880fc5c64566e1
ArpisittNumsri <you@example.com>
2021-09-13 14:48:04 +1000

    Updated Format

--- PRAC/prac_06/used_car.py
+++ PRAC/prac_06/used_car.py
@@ -1,11 +1,8 @@
-"""CP1404/CP5632 Practical - Client code to use the Car class."""
-# Note that the import has a folder (module) in it.
 
 from prac_06.car import Car
 
 
 def main():
-    """Demo test code to show how to use car class."""
     my_car = Car(180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
