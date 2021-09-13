b453e8b64d72be7b3447b845b312c860bfd34fc1
ArpisittNumsri <you@example.com>
2021-09-13 14:36:13 +1000

    Updated Main Repo

---
+++ PRAC/prac_06/guitarclass.py
@@ -0,0 +1,22 @@
+
+YEAR = 2017
+VINTAGE = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE
+
+    def __lt__(self, other):
+        return self.year < other.year
